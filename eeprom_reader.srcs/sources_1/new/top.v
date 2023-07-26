46745cf45f8a26374b00264fef74ab783c201d5e
Anderson Hsieh <andy13579246810@gmail.com>
2023-07-26 19:54:17 -0400

    add top level simulation set and test bench

--- eeprom_reader.srcs/sources_1/new/top.v
+++ eeprom_reader.srcs/sources_1/new/top.v
@@ -1,9 +1,13 @@
 `timescale 1ns / 1ps
 `include "constants.vh"
 
+/*
+  To use this module, scl_reset must be asserted before any signal
+  since reset and start signals are sampled based on SCL
+*/
 module top (
     input            SYS_CLK,  // from on board oscillator to FPGA chip, 200kHz
-    input      [3:0] BTN,      // BTN[0] is reset, BTN[1] is start signal 
+    input      [3:0] BTN,      // BTN[0] = reset, BTN[1] = start, BTN[2] = scl_reset
     input      [3:0] SW,       // SW[0] = HIGH, random read; SW[0] = LOW, sequential read
     inout            SDA,
     output           SCL_OUT,
@@ -15,15 +19,23 @@ module top (
 
 );
   wire done;
-  wire reset = BTN[0];
-  wire start = BTN[1];
-  wire random_read_mode = SW[0];
+  wire reset;
+  wire start;
+  wire random_read_mode;
+  wire scl_reset; 
   reg  SCL_IN;
-  assign LED    = BTN;  //  mapp all buttons to leds
+
+  assign LED[2:0] = BTN[2:0]; 
+  assign LED[3] = done; 
   assign LED0_G = random_read_mode;
   assign LED0_B = !random_read_mode;
+  assign reset = BTN[0];
+  assign start = BTN[1];
+  assign scl_reset = BTN[2];
+  assign random_read_mode = SW[0];
+  
   always @(posedge SYS_CLK) begin
-    if (reset) begin
+    if (scl_reset) begin
       SCL_IN <= 0; // must start at 0 so posedge of SCL lines up with posedge of SYS_CLK
     end else begin
       SCL_IN <= ~SCL_IN;
@@ -32,11 +44,11 @@ module top (
 
   i2c_random_rd_encoder random_encoder_inst (
     .reset(reset),
-    .data_adr(0'b0000000_00000),
-    .device_adr(0'b000),
+    .data_adr(0'b1000000_00000001),
+    .device_adr(0'b101),
     .start(start),
     .double_speed_scl(SYS_CLK),
-    .SCL_IN(SCL),
+    .SCL_IN(SCL_IN),
     .SDA(SDA),
     .SCL_OUT(SCL_OUT),
     .data_out(DATA_OUT),
